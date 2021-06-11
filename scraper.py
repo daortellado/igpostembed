c4a03491a60b2bbd8875873841a7ea354d96e22d
Damian Ortellado <daortellado@gmail.com>
2021-06-11 11:51:49 -0700

    add browser window size due to timeout error

--- scraper.py
+++ scraper.py
@@ -17,6 +17,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
+options.add_argument("window-size=1400,900")
 
 # app and db
 app = flask.Flask(__name__)
